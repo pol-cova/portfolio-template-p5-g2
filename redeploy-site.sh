b4a26ef8ee8511967b829e3f87c86ffbd6498aec
Paul Contreras <p.contreritas22@gmail.com>
2026-07-11 18:45:03 -0600

    refactor(restart): improve the restart script to use service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-DIR_PROJECT=~/portfolio-template-p5-g2
-VENV=$DIR_PROJECT/python3-virtualenv
+DIR_PROJECT="/root/portfolio-template-p5-g2"
+VENV="$DIR_PROJECT/python3-virtualenv"
 
-tmux kill-server 2>/dev/null || true
+cd "$DIR_PROJECT"
 
-cd $DIR_PROJECT
-git fetch && git reset origin/main --hard
+git fetch origin
+git reset --hard origin/main
 
-source $VENV/bin/activate
-pip install -r requirements.txt
+"$VENV/bin/pip" install -r requirements.txt
 
-tmux new-session -d -s flask -c $DIR_PROJECT \
-  "source $VENV/bin/activate && flask run --host=0.0.0.0"
+systemctl restart myportfolio
